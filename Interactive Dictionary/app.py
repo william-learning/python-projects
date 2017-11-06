572708c5d4c779736b8fe76d25a3449295b4145b
William <williamsoftwarecode@gmail.com>
2017-11-06 04:11:24 +0000

    Load JSON data and build scaffold

--- Interactive Dictionary/app.py
+++ Interactive Dictionary/app.py
@@ -7,29 +7,16 @@ Date: 06/11/2017
 This application is a Python based interactive dictionary with a JSON database. 
 Features include: 
     1. A search from a database for word definitions 
-    2. Suggest similar words for inputs which do no exist in the database
+    2. Suggest similar words
     
 Additional Notes:
     1. JSON is a data format with key-value pairs. 
 """
 
-
-
 import json
 
-# Loading the json database into a Python dictionary
+# Import the json database into a Python dictionary
 data = json.load(open("data.json", 'r'))
 
-
-# Looks up and returns word definition from the database
-def search(word):
-    # Accounting for inputs which do not match the database
-    if word in data: 
-        return data[word]
-    else: 
-        return "The word doesn't exist. Please double check it."
-    
-    
-word = input("Enter a word: ")
-# Making the search case insensitive, lowercase for all searches
-print(search(word.lower()))
+search_key = input("Enter a word: ")
+print(data[search_key])
