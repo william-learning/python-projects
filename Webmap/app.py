533acc3b8c2609c23a67527dd2923589d988dca4
William <williamsoftwarecode@gmail.com>
2017-11-14 12:13:54 +0000

    Added authorship statement and updated comments

--- Webmap/app.py
+++ Webmap/app.py
@@ -1,3 +1,22 @@
+"""
+Webmap 
+
+Author: William
+Date: 06/11/2017
+
+https://github.com/williamsoftwarecode/python-projects
+
+This application is a webmap made with Python and Folium, which is a third party library for visualising maps. 
+Features include: 
+    1. Open Street Map (OSM) basemap
+    2. Point markers on top of the basemap
+    
+Additional Notes:
+    1. FeatureGroup can be used to consolidate different attributes together
+"""
+
+
+
 import folium
 
 # Creates an instance of the Map class
@@ -6,10 +25,12 @@ map = folium.Map(location=[38.58,-99.09], zoom_start=6, tiles="Mapbox Bright")
 # Add elements or objects to the map - referred to as child
 fg = folium.FeatureGroup(name="My Map")
 
+# Add multiple markers based on coordinates from a list
 for coordinates in [[38.2, -99.1],[39.2, -97.1]]:
     fg.add_child(folium.Marker(location=coordinates, popup="Hi I am a Marker", icon=folium.Icon(color='green')))
 
 
 map.add_child(fg)
 
+# Create HTML map
 map.save("map.html")
