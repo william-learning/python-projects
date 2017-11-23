b711bae22c48f9a6d2574ba28df28d02712b024b
William <williamsoftwarecode@gmail.com>
2017-11-23 05:52:02 +0000

    Connected search functionality to front end

--- DatabaseApplication/frontend.py
+++ DatabaseApplication/frontend.py
@@ -35,6 +35,11 @@ def view_command():
     for row in backend.view():
         list1.insert(END,row)
 
+def search_command():
+    list1.delete(0,END)
+    for row in backend.search(title_text.get(),author_text.get(),year_text.get(),isbn_text.get()):
+        list1.insert(END,row)
+
 window=Tk()
 
 l1=Label(window,text='Title')
@@ -46,17 +51,17 @@ l3.grid(row=1,column=0)
 l4=Label(window,text='ISBN')
 l4.grid(row=1,column=2)
 
-title=StringVar()
-author=StringVar()
-year=StringVar()
-isbn=StringVar()
-e1=Entry(window,textvariable=title)
+title_text=StringVar()
+author_text=StringVar()
+year_text=StringVar()
+isbn_text=StringVar()
+e1=Entry(window,textvariable=title_text)
 e1.grid(row=0,column=1)
-e2=Entry(window,textvariable=author)
+e2=Entry(window,textvariable=author_text)
 e2.grid(row=0,column=3)
-e3=Entry(window,textvariable=year)
+e3=Entry(window,textvariable=year_text)
 e3.grid(row=1,column=1)
-e4=Entry(window,textvariable=isbn)
+e4=Entry(window,textvariable=isbn_text)
 e4.grid(row=1,column=3)
 
 list1=Listbox(window,height=6,width=35)
@@ -70,7 +75,7 @@ sb1.configure(command=list1.yview)
 
 b1=Button(window,text="View all",width=12,command=view_command)
 b1.grid(row=2,column=3)
-b1=Button(window,text="Search entry",width=12)
+b1=Button(window,text="Search entry",width=12,command=search_command)
 b1.grid(row=3,column=3)
 b1=Button(window,text="Add entry",width=12)
 b1.grid(row=4,column=3)
